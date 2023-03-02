1ee4212f3d96fd5b12b2e404f768c3382b4c6f8e
KaijianHuang <112913171+KaijianHuang@users.noreply.github.com>
2023-03-02 01:26:02 +0000

    rock-paper-scissors game

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,69 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
-    let mut grades = Vec::new();
-    let mut credit_hours = Vec::new();
+    println!("Let's play Rock-Paper-Scissors!");
+    loop {
+        println!("Enter your choice (rock, paper, or scissors):");
+        let mut player_choice = String::new();
+        io::stdin()
+            .read_line(&mut player_choice)
+            .expect("Failed to read line");
 
-    // Get grades and credit from user
-    for i in 1.. {
-        println!("Enter grade for class {} (or 'q' to quit)", i);
-        let mut grade = String::new();
-        std::io::stdin().read_line(&mut grade).unwrap();
-        grade = grade.trim().to_uppercase();
-
-        if grade == "Q" {
-            break;
+        let player_choice = player_choice.trim();
+        if player_choice != "rock" && player_choice != "paper" && player_choice != "scissors" {
+            println!("Invalid input. Please try again.");
+            continue;
         }
 
-        let grade = match grade.as_str() {
-            "A+" => 4.0,"A" => 4.0,"A-" => 3.7,
-            "B+" => 3.3,"B" => 3.0,"B-" => 2.7,
-            "C+" => 2.3,"C" => 2.0,"C-" => 1.7,
-            "F" => 0.0,"NC"=>0.0,
-            _ => {
-                println!("Invalid grade, try again");
-                continue;
-            }
-        };
+        let computer_choice = get_computer_choice();
+        println!(
+            "You chose {} and the computer chose {}.",
+            player_choice, computer_choice
+        );
 
-        println!("Enter credit for this course {}:", i);
-        let mut credit_hour = String::new();
-        std::io::stdin().read_line(&mut credit_hour).unwrap();
-        let credit_hour: f32 = match credit_hour.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Invalid credit, try again");
-                continue;
-            }
-        };
+        let result = get_game_result(player_choice, computer_choice);
+        match result {
+            GameResult::Win => println!("You win!"),
+            GameResult::Lose => println!("You lose!"),
+            GameResult::Draw => println!("It's a draw."),
+        }
 
-        grades.push(grade);
-        credit_hours.push(credit_hour);
+        println!("Do you want to play again? (yes or no)");
+        let mut play_again = String::new();
+        io::stdin()
+            .read_line(&mut play_again)
+            .expect("Failed to read line");
+        if play_again.trim() != "yes" {
+            break;
+        }
     }
+}
 
-    // Calculate GPA
-    let mut total_quality_points = 0.0;
-    let mut total_credit_hours = 0.0;
-    for i in 0..grades.len() {
-        total_quality_points += grades[i] * credit_hours[i];
-        total_credit_hours += credit_hours[i];
+enum GameResult {
+    Win,
+    Lose,
+    Draw,
+}
+
+fn get_computer_choice() -> String {
+    let choices = ["rock", "paper", "scissors"];
+    let random_index = rand::thread_rng().gen_range(0..3);
+    choices[random_index].to_string()
+}
+
+fn get_game_result(player_choice: &str, computer_choice: String) -> GameResult {
+    if player_choice == "rock" && computer_choice == "scissors"
+        || player_choice == "paper" && computer_choice == "rock"
+        || player_choice == "scissors" && computer_choice == "paper"
+    {
+        GameResult::Win
+    } else if player_choice == "rock" && computer_choice == "paper"
+        || player_choice == "paper" && computer_choice == "scissors"
+        || player_choice == "scissors" && computer_choice == "rock"
+    {
+        GameResult::Lose
+    } else {
+        GameResult::Draw
     }
-
-    let gpa = total_quality_points / total_credit_hours;
-    println!("GPA: {:.2}", gpa);
 }
