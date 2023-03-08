1f46c14a630b0b4b639d87af09c100343e7de103
KaijianHuang <112913171+KaijianHuang@users.noreply.github.com>
2023-03-07 21:39:39 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,18 @@
-use rand::Rng;
-use std::io;
+use rand::seq::SliceRandom;
 
 fn main() {
-    println!("Let's play Rock-Paper-Scissors!");
-    loop {
-        println!("Enter your choice (rock, paper, or scissors):");
-        let mut player_choice = String::new();
-        io::stdin()
-            .read_line(&mut player_choice)
-            .expect("Failed to read line");
+    let cookie_messages = vec![
+        "You will have a great day today!",
+        "Good news is coming your way!",
+        "You will soon receive a promotion!",
+        "The stars will align in your favor!",
+        "You will find true love soon!",
+        "Your creativity will blossom!",
+        "You will receive unexpected blessings!",
+        "Your hard work will pay off!",
+    ];
 
-        let player_choice = player_choice.trim();
-        if player_choice != "rock" && player_choice != "paper" && player_choice != "scissors" {
-            println!("Invalid input. Please try again.");
-            continue;
-        }
-
-        let computer_choice = get_computer_choice();
-        println!(
-            "You chose {} and the computer chose {}.",
-            player_choice, computer_choice
-        );
-
-        let result = get_game_result(player_choice, computer_choice);
-        match result {
-            GameResult::Win => println!("You win!"),
-            GameResult::Lose => println!("You lose!"),
-            GameResult::Draw => println!("It's a draw."),
-        }
-
-        println!("Do you want to play again? (yes or no)");
-        let mut play_again = String::new();
-        io::stdin()
-            .read_line(&mut play_again)
-            .expect("Failed to read line");
-        if play_again.trim() != "yes" {
-            break;
-        }
-    }
-}
-
-enum GameResult {
-    Win,
-    Lose,
-    Draw,
-}
-
-fn get_computer_choice() -> String {
-    let choices = ["rock", "paper", "scissors"];
-    let random_index = rand::thread_rng().gen_range(0..3);
-    choices[random_index].to_string()
-}
-
-fn get_game_result(player_choice: &str, computer_choice: String) -> GameResult {
-    if player_choice == "rock" && computer_choice == "scissors"
-        || player_choice == "paper" && computer_choice == "rock"
-        || player_choice == "scissors" && computer_choice == "paper"
-    {
-        GameResult::Win
-    } else if player_choice == "rock" && computer_choice == "paper"
-        || player_choice == "paper" && computer_choice == "scissors"
-        || player_choice == "scissors" && computer_choice == "rock"
-    {
-        GameResult::Lose
-    } else {
-        GameResult::Draw
-    }
+    let mut rng = rand::thread_rng();
+    let message = cookie_messages.choose(&mut rng).unwrap();
+    println!("Lucky cookie message: {}", message);
 }
