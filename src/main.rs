7c6f987141bc5dc952278d2c3d1f64131c6bf8ba
KaijianHuang <112913171+KaijianHuang@users.noreply.github.com>
2023-03-21 15:50:05 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-
 use std::io;
+use rand::Rng;
 
 fn main() {
-    // Prompt the user to enter a range
-    println!("Enter a range of numbers:");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    
-    // Parse the input range
-    let range: Vec<&str> = input.trim().split('-').collect();
-    let start = range[0].parse::<i32>().unwrap();
-    let end = range[1].parse::<i32>().unwrap();
+    println!("Enter the start of the range:");
+    let mut start = String::new();
+    io::stdin().read_line(&mut start).expect("Failed to read start number.");
+    let start: u32 = start.trim().parse().expect("Please enter a valid number.");
 
-    // Generate a random lucky number within the range
-    let lucky_number = rand::thread_rng().gen_range(start, end);
-    
-    // Print the lucky number
-    println!("Your lucky number is {}", lucky_number);
+    println!("Enter the end of the range:");
+    let mut end = String::new();
+    io::stdin().read_line(&mut end).expect("Failed to read end number.");
+    let end: u32 = end.trim().parse().expect("Please enter a valid number.");
+
+    let range = start..=end;
+    let lucky_number = rand::thread_rng().gen_range(range);
+    println!("Your lucky number is: {}", lucky_number);
 }
