806abf34efc0983ed589311dacb47c2fe981b61a
KaijianHuang <112913171+KaijianHuang@users.noreply.github.com>
2023-03-31 14:45:09 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 use std::io;
-use rand::Rng;
 
 fn main() {
-    println!("Enter the start of the range:");
-    let mut start = String::new();
-    io::stdin().read_line(&mut start).expect("Failed to read start number.");
-    let start: u32 = start.trim().parse().expect("Please enter a valid number.");
+    println!("Enter the name of a popular city(new york city, london, paris, tokyo):");
+    let mut city = String::new();
+    io::stdin().read_line(&mut city).expect("Failed to read input.");
 
-    println!("Enter the end of the range:");
-    let mut end = String::new();
-    io::stdin().read_line(&mut end).expect("Failed to read end number.");
-    let end: u32 = end.trim().parse().expect("Please enter a valid number.");
-
-    let range = start..=end;
-    let lucky_number = rand::thread_rng().gen_range(range);
-    println!("Your lucky number is: {}", lucky_number);
+    match city.trim().to_lowercase().as_str() {
+        "new york city" => println!("New York City is the largest city in the United States, with a population of over 8 million people. It is a global center for finance, media, art, and culture."),
+        "london" => println!("London is the capital and largest city of England and the United Kingdom, with a population of over 8 million people. It is a global center for finance, education, healthcare, media, and the arts."),
+        "paris" => println!("Paris is the capital and largest city of France, with a population of over 2 million people. It is known as the City of Light, and is famous for its art, fashion, cuisine, and historic landmarks such as the Eiffel Tower and the Louvre Museum."),
+        "tokyo" => println!("Tokyo is the capital and largest city of Japan, with a population of over 13 million people. It is a global center for finance, technology, entertainment, and fashion."),
+        _ => println!("Sorry, I don't have an introduction for that city."),
+    }
 }
